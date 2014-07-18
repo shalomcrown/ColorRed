4b926f05d2f424f1543d9bbf0bab1bd42f101015
shalomcrown <shalomc@infinidat.com>
2014-07-18 16:38:34 +0300

    Initial code for GUI

--- src/main/java/com/kirayim/colored/colored/Main.java
+++ src/main/java/com/kirayim/colored/colored/Main.java
@@ -1,5 +1,11 @@
 package com.kirayim.colored.colored;
 
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -9,6 +15,13 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.TimeZone;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
@@ -27,13 +40,20 @@ import com.ibm.icu.text.Bidi;
  * Hello world!
  *
  */
-public class Main implements Runnable {
+public class Main {
     Thread updateThread = null;
     public static final String orefUrl = "http://www.oref.org.il/WarningMessages/alerts.json";
-    String lastId = "--";
-    String requiredArea = "160";
+    String lastId = null;
+    volatile String requiredArea = "160";
     URL sirenUrl = null;
     long lastModified = 0L;
+    private JFrame frmColorRed;
+    private JTextField textField;
+    private JList<String> alertList;
+    ArrayList<String> listData = new ArrayList<>();
+
+
+    // ===================================================================
 
     public Main(Integer areaCode) {
         sirenUrl = getClass().getResource("/91244-SIREN2.mp3");
@@ -47,18 +67,98 @@ public class Main implements Runnable {
 
         System.out.println("Working on area: " + requiredArea);
 
-        updateThread = new Thread(this, "Update thread");
-        updateThread.start();
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    initialize();
+                    frmColorRed.setVisible(true);
+
+                    updateThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            updateThread();
+                        }
+                    }, "Update thread");
+
+                    updateThread.start();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     // ===================================================================
 
-    public void run() {
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frmColorRed = new JFrame();
+        frmColorRed.setTitle("Color Red - area " + requiredArea);
+        frmColorRed.setBounds(100, 100, 450, 300);
+        frmColorRed.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        gridBagLayout.columnWidths = new int[]{0, 0, 0};
+        gridBagLayout.rowHeights = new int[]{0, 0, 0};
+        gridBagLayout.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+        gridBagLayout.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+        frmColorRed.getContentPane().setLayout(gridBagLayout);
+
+        textField = new JTextField(requiredArea);
+        GridBagConstraints gbc_textField = new GridBagConstraints();
+        gbc_textField.insets = new Insets(0, 0, 5, 5);
+        gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_textField.gridx = 0;
+        gbc_textField.gridy = 0;
+        frmColorRed.getContentPane().add(textField, gbc_textField);
+        textField.setColumns(10);
+
+        JButton btnNewButton = new JButton("Change Area");
+        btnNewButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                requiredArea = textField.getText();
+                frmColorRed.setTitle("Color Red - area " + requiredArea);
+            }
+        });
+        GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+        gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+        gbc_btnNewButton.gridx = 1;
+        gbc_btnNewButton.gridy = 0;
+        frmColorRed.getContentPane().add(btnNewButton, gbc_btnNewButton);
+
+        JScrollPane scrollPane = new JScrollPane();
+        GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+        gbc_scrollPane.gridwidth = 2;
+        gbc_scrollPane.insets = new Insets(0, 0, 0, 5);
+        gbc_scrollPane.fill = GridBagConstraints.BOTH;
+        gbc_scrollPane.gridx = 0;
+        gbc_scrollPane.gridy = 1;
+        frmColorRed.getContentPane().add(scrollPane, gbc_scrollPane);
+
+        alertList = new JList<>();
+        scrollPane.setViewportView(alertList);
+    }
+
+    private void addListElement(final String data) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                listData.add(0, data);
+                alertList.setListData(listData.toArray(new String[0]));
+            }
+        });
+    }
+
+    // ===================================================================
+
+    public void updateThread() {
         ObjectMapper mapper = new ObjectMapper();
 
         try {
             while (true) {
-
+                boolean alert = false;
                 URL url = new URL(orefUrl);
 
                 try {
@@ -87,7 +187,7 @@ public class Main implements Runnable {
 
                         String newId =(String) map.get("id");
 
-                        if (! newId.equals(lastId)) {
+                        if (lastId == null || ! newId.equals(lastId)) {
                             long idTime = Long.parseLong(newId);
                             int offset = TimeZone.getDefault().getOffset(new Date().getTime());
                             idTime -= offset;
@@ -99,13 +199,15 @@ public class Main implements Runnable {
                             Bidi bidi = new Bidi();
                             bidi.setReorderingMode(Bidi.REORDER_DEFAULT);
                             bidi.setPara(reconstructedJson, Bidi.LEVEL_DEFAULT_RTL, new byte[reconstructedJson.length() * 2]);
-
                             System.out.println(bidi.writeReordered(Bidi.DO_MIRRORING));
 
                             @SuppressWarnings("unchecked")
                             ArrayList<String> data = (ArrayList<String>)map.get("data");
 
                             if (data != null && data.size() > 0) {
+                                StringBuffer buffer = new StringBuffer();
+                                boolean firstname = true;
+
                                 for (String line:data) {
                                     for (String innerSplit : line.split(",")) {
 
@@ -113,8 +215,17 @@ public class Main implements Runnable {
 
                                         int lastSpace = innerSplit.lastIndexOf(' ');
                                         String lastField = innerSplit.substring(lastSpace + 1);
+                                        String placename = innerSplit.substring(0, lastSpace - 1);
+
+                                        if (firstname) {
+                                            firstname = false;
+                                        } else {
+                                            buffer.append(", ");
+                                        }
+                                        buffer.append(placename);
 
                                         if (lastField.equals(requiredArea)) {
+                                            alert = true;
                                             System.out.println("**** ALERT ALERT ALERT ****");
                                             InputStream sitenIn = sirenUrl.openStream();
                                             Player player = new Player(sitenIn);
@@ -122,6 +233,18 @@ public class Main implements Runnable {
                                         }
                                     }
                                 }
+                                bidi = new Bidi();
+                                bidi.setReorderingMode(Bidi.REORDER_DEFAULT);
+                                bidi.setPara(buffer.toString(), Bidi.LEVEL_DEFAULT_RTL, new byte[buffer.length() * 2]);
+                                String output = lastTime.toString() + "\n" + bidi.writeReordered(Bidi.DO_MIRRORING);
+
+                                if (alert) {
+                                    output += " ****** ";
+                                }
+
+                                alertList.add(new JTextArea(output), 0);
+                            } else if (lastId == null) {
+                                addListElement(lastTime.toString() + "\n No alerts");
                             }
 
                             lastId = newId;
@@ -185,6 +308,7 @@ public class Main implements Runnable {
             areaCode = Integer.parseInt(cmdLine.getOptionValue('a'));
         }
 
-        new Main(areaCode);
+        Main window = new Main(areaCode);
+
     }
 }
